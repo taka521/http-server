a501641ce657bf147c579f015e16a787587619fc
taka <521taka.m@gmail.com>
2018-02-17 14:37:01 +0900

    ヘッダをボディを別々に取得するように変更

--- Java/httpserver/src/main/java/server/HttpServer.java
+++ Java/httpserver/src/main/java/server/HttpServer.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.CharBuffer;
 import java.util.logging.Logger;
 
 /**
@@ -41,7 +42,34 @@ public class HttpServer {
              Socket socket = server.accept();
              BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
-            reader.lines().forEach(System.out::println);
+            String line = reader.readLine();
+            StringBuffer header = new StringBuffer();
+            int contentLength = 0;
+
+            // Content-Lengthの値を取得
+            while (line != null && !line.isEmpty()) {
+                if (line.startsWith("Content-Length")) {
+                    contentLength = Integer.parseInt(line.split(":")[1].trim());
+                }
+                header.append(line).append("\n");
+                line = reader.readLine();
+            }
+
+            // 空行を発見したら、以降はボディ部になる
+
+            StringBuffer body = new StringBuffer();
+
+            // ヘッダの Content-Length からボディ部を読み込むバイト数を取得し、
+            // 指定されたバイス数のデータを取得する。
+            if (contentLength > 0) {
+                char[] c = new char[contentLength];
+                reader.read(c);
+                body.append(c);
+            }
+
+            System.out.println(header);
+            System.out.println(body);
+
         }
 
         logger.info("=== server stop === ");
